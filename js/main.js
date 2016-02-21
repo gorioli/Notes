8c0bbd21587f2f1772a22e59ffdea4938c25baea
gorioli <gorev.olga@gmail.com>
2016-02-21 14:35:57 -0800

    First commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+//var data = require('./data.js');
+//var ContentBox = require('./components/ContentBox.jsx');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import data from './data.js';
+import ContentBox from  './components/ContentBox.jsx';
+
+(function main() {
+
+    // es6 works!
+    var evens = [1, 3, 5, 7, 9];
+    var nums = evens.map((v, i) => v + i);
+    console.log(evens, nums);
+
+    var fives = [];
+    nums.forEach(v => {
+        if (v % 5 === 0)
+            fives.push(v);
+    });
+    console.log(fives);
+
+    var {b, a } = { x: 1, y: 2, a: 3, b: 4 };
+    console.log(a, b);
+
+    ReactDOM.render(
+        <ContentBox data={data}/>,
+        document.getElementById('content')
+    );
+})();
